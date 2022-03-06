312e5fd73296fbcd24c039fb9f1889038e319799
BisiDavi <oludavidconnect@gmail.com>
2022-03-06 21:41:07 +0100

    style csp nonce updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import flush from "styled-jsx/server";
+import { v4 as uuidv4 } from "uuid";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const nonce = uuidv4();
+
+    // https://github.com/vercel/next.js/blob/canary/packages/next/pages/_document.tsx#L89
+    const { html, head } = await ctx.renderPage();
+    // Adds `nonce` for style tags on Server Side Rendering
+    const styles = [...flush({ nonce })];
+
+    let contentSecurityPolicy = "";
+    if (process.env.NODE_ENV === "production") {
+      contentSecurityPolicy = `style-src 'nonce-${nonce}';`;
+    } else {
+      // react-refresh needs 'unsafe-eval'
+      // Next.js needs 'unsafe-inline' during development https://github.com/vercel/next.js/blob/canary/packages/next/client/dev/fouc.js
+      // Specifying 'nonce' makes a modern browsers ignore 'unsafe-inline'
+      contentSecurityPolicy = `default-src 'self'; style-src 'unsafe-inline'; script-src 'self' 'unsafe-eval';`;
+    }
+
+    ctx.res.setHeader("Content-Security-Policy", contentSecurityPolicy);
+
+    return { styles, html, head, nonce };
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -11,6 +36,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
             crossOrigin="true"
           />
+          <meta property="csp-nonce" content={this.props.nonce} />
         </Head>
         <Script
           onLoad={() => console.log("vbout-loaded")}
