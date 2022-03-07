07825c5d16b7bac5f69cdf0bb0a46f9adb481637
BisiDavi <oludavidconnect@gmail.com>
2022-03-07 02:23:16 +0100

    homepage updated

--- src/components/Blog/BlogBanner.tsx
+++ src/components/Blog/BlogBanner.tsx
@@ -1,19 +1,25 @@
+import Link from "next/link";
+
 export default function BlogBanner() {
   return (
     <section className="w-full items-center justify-center">
       <div className="flex justify-between">
-        <div className="w-1/2 bg-pink-300 hover:bg-pink-200 flex justify-center p-8 items-center  md:p-16">
-          <div className="content">
-            <h3 className="capitalize text-lg font-bold">Read the Blog</h3>
-            <p>Latest store, Fashion news and trends</p>
-          </div>
-        </div>
-        <div className="w-1/2 bg-purple-300 hover:bg-purple-200 flex justify-center  p-8 items-center md:p-16">
-          <div className="content">
-            <h3 className="capitalize font-bold text-lg">Read the Blog</h3>
-            <p>Latest store, Fashion news and trends</p>
-          </div>
-        </div>
+        <Link href="/blog" passHref>
+          <a className="w-1/2 bg-pink-300 hover:bg-pink-200 flex justify-center p-8 items-center  md:p-16">
+            <div className="content">
+              <h3 className="capitalize text-lg font-bold">Read the Blog</h3>
+              <p>Latest store, Fashion news and trends</p>
+            </div>
+          </a>
+        </Link>
+        <Link href="/contact-us" passHref>
+          <a className="w-1/2 bg-purple-300 hover:bg-purple-200 flex justify-center  p-8 items-center md:p-16">
+            <div className="content">
+              <h3 className="capitalize font-bold text-lg">Read the Blog</h3>
+              <p>Latest store, Fashion news and trends</p>
+            </div>
+          </a>
+        </Link>
       </div>
     </section>
   );
